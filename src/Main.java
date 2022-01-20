3c3cdabb429799071ec2469ded32c2bf835a76e9
leonei <leoneisilva.lg@gmail.com>
2022-01-20 11:05:37 -0300

    Curso e Mentoria de Java com POO

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -22,9 +24,41 @@ public class Main {
         mentoria.setDescricao("Aprofundando em POO com Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Abstraindo um Bootcamp usando Orientação a Objetos em Java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLeonei = new Dev();
+        devLeonei.setNome("Leonei");
+        devLeonei.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Leonei:" + devLeonei.getConteudosInscritos());
+        devLeonei.progredir();
+        devLeonei.progredir();
+        System.out.println("<********************************************************>");
+        System.out.println("Conteúdos inscritos Leonei:" + devLeonei.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Leonei:" + devLeonei.getConteudosConcluido());
+        System.out.println("XP: " + devLeonei.calcularTotalXp());
+
+        System.out.println("<===========================================================>");
+
+        Dev devLiliane = new Dev();
+        devLiliane.setNome("Liliane");
+        devLiliane.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Liliane:" + devLiliane.getConteudosInscritos());
+        devLiliane.progredir();
+        devLiliane.progredir();
+        devLiliane.progredir();
+        System.out.println("<************************************************************>");
+        System.out.println("Conteúdos inscritos Liliane:" + devLiliane.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Liliane:" + devLiliane.getConteudosConcluido());
+        System.out.println("XP: " + devLiliane.calcularTotalXp());
 
     }
 }
